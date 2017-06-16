f29140097a791253b6a841a6b70c8ef5e6e9c6e0
Sahana <gunaseks@mail.gvsu.edu>
2017-06-15 21:21:20 -0700

    Sample code that returns hello world 10 times - MVC

---
+++ Sample.API/Sample.API/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("")]
+        [Route("ReturnString")]
+        [HttpGet]
+        public string ReturnString()
+        {
+            string str = "Hello World";
+            return str;
+            //return Json(new { str }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Sample.API/Sample.API/Scripts/JQueryFunctions.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('#btnTest').click(function () {
+        $.get("/Home/ReturnString", null, function (data) {
+
+            for (var i = 0; i < 10; i++) {
+                var row = $('<tr><td>' + data + '</td></tr>');
+                $('#myTable').append(row);
+            }
+        });
+    });
+});
